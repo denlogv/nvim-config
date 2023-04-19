551ff2344084bbbee8ff7dcb0925eec93ac5732e
Denis Logvinenko <logvinenko@libra.de>
2023-04-19 17:38:37 +0200

    Added outline, simplified specs for Python debugging options

---
+++ lua/plugins/debuggers.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "nvim-telescope/telescope-dap.nvim",
+        config = function()
+            require("telescope").load_extension("dap")
+        end,
+    },
+    {
+        "mfussenegger/nvim-dap-python",
+        config = function()
+            require("dap-python").setup("python")
+        end,
+    },
+}
--- lua/plugins/debugging.lua
+++
@@ -1,4 +0,0 @@
-return {
-  { "nvim-telescope/telescope-dap.nvim" },
-  { "mfussenegger/nvim-dap-python" },
-}
