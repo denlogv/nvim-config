3d7ad1eb482d6e808a86ffe57021906309dbaeeb
Denis Logvinenko <logvinenko@libra.de>
2023-08-25 11:26:29 +0200

    Modified lspconfig settings

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,8 @@
 return {
     { "echasnovski/mini.pairs", enabled = true },
-    { "ggandor/leap.nvim",      enabled = false },
-    { "ggandor/flit.nvim",      enabled = false },
-    { "folke/flash.nvim",       enabled = false },
+    { "ggandor/leap.nvim", enabled = false },
+    { "ggandor/flit.nvim", enabled = false },
+    { "folke/flash.nvim", enabled = false },
+    -- { "folke/neoconf.nvim", enabled = false },
+    -- { "folke/neodev.nvim", enabled = false },
 }
